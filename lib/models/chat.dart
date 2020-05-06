f41f895a4ebd66ceb8a3f2e9fce26ad1bb3184ad
Doğukan <dogukandogu8@gmail.com>
2020-05-07 01:50:34 +0300

    whatsappclone-callscreen

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -2,9 +2,10 @@ class Chat {
   String name;
   String message;
   String time;
+  String call;
   String avatarUrl;
 
-  Chat({this.name, this.message, this.time, this.avatarUrl});
+  Chat({this.name, this.message, this.time,this.call, this.avatarUrl});
 }
 
 List<Chat> fakeData = [
@@ -12,24 +13,28 @@ List<Chat> fakeData = [
         name: "Doğukan",
         message: "Yov wazap!",
         time: "13:30",
+        call:"Dün 16:01",
         avatarUrl:
             "https://camo.githubusercontent.com/8234a16941f0d9bd25ad5882e0e7d0f643331983/68747470733a2f2f7261772e6769746875622e636f6d2f736568726775742f6e6f64652d7265747269636f6e2f6d61737465722f6578616d706c65732f696d616765732f6769746875622e706e67"),
     Chat(
         name: "Ali İhsan",
         message: "Şu kodu yaz!",
         time: "14:30",
+        call:"2 Mayıs 01:08",
         avatarUrl:
             "https://camo.githubusercontent.com/8234a16941f0d9bd25ad5882e0e7d0f643331983/68747470733a2f2f7261772e6769746875622e636f6d2f736568726775742f6e6f64652d7265747269636f6e2f6d61737465722f6578616d706c65732f696d616765732f6769746875622e706e67"),
     Chat(
         name: "Fikri",
         message: "Yayınmayın!",
         time: "15:30",
+        call:"27 Nisan 16:41",
         avatarUrl:
             "https://camo.githubusercontent.com/8234a16941f0d9bd25ad5882e0e7d0f643331983/68747470733a2f2f7261772e6769746875622e636f6d2f736568726775742f6e6f64652d7265747269636f6e2f6d61737465722f6578616d706c65732f696d616765732f6769746875622e706e67"),
     Chat(
         name: "Burkay",
         message: "!",
         time: "16:30",
+        call:"17 Şubat 11:01",
         avatarUrl:
             "https://camo.githubusercontent.com/8234a16941f0d9bd25ad5882e0e7d0f643331983/68747470733a2f2f7261772e6769746875622e636f6d2f736568726775742f6e6f64652d7265747269636f6e2f6d61737465722f6578616d706c65732f696d616765732f6769746875622e706e67"),
   ];
--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/chat.dart';
 
-class CallScreen extends StatelessWidget {
+class CallScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CallScreenState();
+}
+
+class CallScreenState extends State {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Aramalar",style:TextStyle(fontSize: 20)),
+    return ListView.builder(
+      itemCount: fakeData.length,
+      itemBuilder: (context, i) => Column(
+        children: <Widget>[
+          Divider(
+            height: 10.0,
+          ),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(fakeData[i].avatarUrl),
+            ),
+            title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+              Text(
+                fakeData[i].name,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Icon(Icons.videocam),
+            ]),
+            subtitle: Container(
+              padding: EdgeInsets.only(top: 5.0),
+              child: Text(
+                fakeData[i].call,
+                style: TextStyle(color: Colors.grey, fontSize: 15.0),
+              ),
+              
+            ),
+          )
+        ],
+      ),
     );
   }
-  
 }
